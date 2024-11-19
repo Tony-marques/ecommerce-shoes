2f95c02bba96bdcf767e12031c8c0dd29166e186
Tony Marques <tony.marques@live.fr>
2024-11-19 14:04:40 +0100

    Add style in <Navbar/>

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -1,5 +1,6 @@
 import { IoIosHeartEmpty } from "react-icons/io";
 import { IoCartOutline, IoPeople } from "react-icons/io5";
+import styled from "styled-components";
 
 export default function Navbar() {
   return (
@@ -17,6 +18,37 @@ export default function Navbar() {
 }
 
 const NavbarStyled = styled.div`
-  
+  display: flex;
+  justify-content: center;
+  border-bottom: 1px solid #f1f5f9;
+  padding: 1rem;
 
-`
+  nav {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    max-width: 1000px;
+
+    input {
+      display: flex;
+      background-color: #f1f5f9;
+      padding: 0.75rem 1rem;
+      width: 250px;
+      outline: none;
+      border: none;
+      border-radius: 5px;
+    }
+
+    .button-container {
+      display: flex;
+      gap: 1rem;
+
+      svg {
+        width: 20px;
+        height: 20px;
+        cursor: pointer;
+      }
+    }
+  }
+`;
